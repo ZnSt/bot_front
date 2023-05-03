9e5f4d22fab260fc81a831cc4ab8cfaeb2c2b702
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2023-05-03 11:36:06 +0300

    add btn and header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import { useEffect } from 'react';
 import './App.css';
-const tg = window.Telegram.WebApp;
 
 const App = () => {
   useEffect(() => {
     tg.ready();
   }, []);
 
-  const onClose = () => {
-    tg.close();
-  };
   return (
     <div>
       <button onClick={onClose}>Закрыть</button>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+import Button from '../Button/Button';
+
+export const Header = () => {
+  const tg = window.Telegram.WebApp;
+
+  const onClose = () => {
+    tg.close();
+  };
+  return (
+    <div className="header">
+      <Button onClick={onClose}>Закрыть</Button>
+      <span className="username">{tg.initDataUnsafe?.user?.username}</span>
+    </div>
+  );
+};
